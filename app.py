070791150f86ca21de4f631e3a8921db63a5882c
Tushar-Chaudhari-04 <tushar.c8942@gmail.com>
2024-11-14 11:54:02 +0530

    app.py changes done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask,render_template,request,jsonify,json
 import pandas as pd
 import numpy as np
 import pickle
@@ -11,27 +11,31 @@ __model=None
 __location=None
 __data_columns=None
 
-df=pd.read_csv('notebook/data/bengaluru_house_cleaned_csv')
-__location=df.columns[4:]
-__data_columns=df.columns.drop('price')
+# df=pd.read_csv('notebook/data/bengaluru_house_cleaned_csv')
 
 def load_save_artifacts():
-   with open('notebook/banglore_home_prices_model.pickle','rb') as file:
+   with open('banglore_home_prices_model.pickle','rb') as file:
        global __model
-       if __model is None:
-        __model=pickle.load(file)
-        print("model",__model)
+       __model=pickle.load(file)
+
+   with open('columns.json','rb') as f1:
+      global __location
+      global __data_columns
+      __data_columns=json.load(f1)
+
+      if __data_columns and 'data' in __data_columns:
+         __location = __data_columns['data'][4:]  # Assign __location to be a slice of the 
+      else:
+         print("Error: __data_columns is empty or 'data' key is missing.")
+      # __data_columns=__location['data']
 
 def get_estimate_price(bath,bhk,square_feet,location):
-   # global __data_columns
+   
    try:
-      print("location",location)
-      print("__data_columns[location]",__data_columns.get_loc(location))
-      loc_index=__data_columns.get_loc(location)
-      print("loc_index",loc_index)
+      loc_index=__data_columns['data'].index(location)
    except:
       loc_index=-1
-   x=np.zeros(len(__data_columns))
+   x=np.zeros(len(__data_columns['data']))
    x[0]=bath
    x[1]=bhk
    x[2]=square_feet
@@ -39,7 +43,6 @@ def get_estimate_price(bath,bhk,square_feet,location):
    if loc_index>=0:
       x[loc_index]=1
 
-   print("x[loc_index]",loc_index,x[loc_index],x,len(x))
    return round(__model.predict([x])[0],2)
 
 
@@ -61,7 +64,6 @@ def predict_home_price():
    )
 
    response.headers.add('Access-Control-Allow-Origin','*')
-   print("response",response)
    return response
 
 
